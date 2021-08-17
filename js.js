c6f5705ae854c0398c2a2c7089c87c934f8e3658
ntjoel19 <ntjoel19@gmail.com>
2021-08-17 11:36:49 +0100

    test fishing

---
+++ js.js
@@ -0,0 +1,8 @@
+var win = window,
+    doc = document,
+    docElem = doc.documentElement,
+    body = doc.getElementsByTagName('body')[0],
+    x = win.innerWidth || docElem.clientWidth || body.clientWidth,
+    y = win.innerHeight|| docElem.clientHeight|| body.clientHeight;
+alert(y+'px');
+document.getElementsById('body').style.height = 700;
